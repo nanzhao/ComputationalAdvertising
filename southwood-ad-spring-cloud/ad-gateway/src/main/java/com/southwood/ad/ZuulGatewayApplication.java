4f8566e2460932370693b029982db2b2698b4203
zhaonan25 <zhaonan8@jd.com>
2019-02-10 00:58:38 +0800

    add init gateway

---
+++ southwood-ad-spring-cloud/ad-gateway/src/main/java/com/southwood/ad/ZuulGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.southwood.ad;
+
+import org.bouncycastle.math.ec.custom.sec.SecT571R1Point;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication   //网关的功能在cloud里面
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
