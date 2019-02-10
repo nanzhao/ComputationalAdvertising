10b135a7742440a37ab63bfcba3e27bf16760894
zhaonan25 <zhaonan8@jd.com>
2019-02-10 09:49:13 +0800

    add zull proxy

---
+++ southwood-ad-spring-cloud/ad-gateway/src/main/java/com/southwood/ad/filter/AccessLogFilter.java
@@ -0,0 +1,43 @@
+package com.southwood.ad.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class AccessLogFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        //数据越小先执行，这里是最后执行
+        return FilterConstants.SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        Long startTime = (Long) context.get("startTime");
+        String uri = request.getRequestURI();
+        long duration = System.currentTimeMillis() - startTime;
+
+        log.info("uri: " + uri + ", duration: " + duration / 100 + "ms");
+        return null;
+    }
+}
